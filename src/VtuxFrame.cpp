db90b1a4ee05e5fb065f2ebc0919f5aa6dfd1c2c
grivetti <grivetti.dev@tutanota.com>
2021-10-15 14:12:50 -0300

    added metadata about the license in the files

--- src/VtuxFrame.cpp
+++ src/VtuxFrame.cpp
@@ -1,3 +1,17 @@
+/***************************
+ *
+ * THIS FILE IS PART OF THE VISUAL TUX SOFTWARE OR VTUX
+ * 
+ * GNU AFFERO GENERAL PUBLIC LICENSE
+ * 
+ * VTUX is free software, you can study, fork and modify
+ * as long you respect the license imposed by its creator;
+ * VTUX is distributed in the hope that it will be useful
+ * for every kind of developer, but it doesn't come without
+ * any warranty;
+ * =)
+*****************************/
+
 #include "VtuxFrame.hpp"
 
 
